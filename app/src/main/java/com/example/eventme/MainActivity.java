b3715584bb9cb6d8ab00e71bcb4d6c1b8d998b1e
Mourad Zeynalov <zeynalov@usc.edu>
2022-10-26 18:02:14 -0700

    Navbar Complete

--- app/src/main/java/com/example/eventme/MainActivity.java
+++ app/src/main/java/com/example/eventme/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.eventme;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
-class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.example.eventme.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         //Initialize the bottom navigation view
         //create bottom navigation view object
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view);
-        val navController = findNavController(R.id.nav_fragment);
-        bottomNavigationView.setupWithNavController(navController);
+        BottomNavigationView bottomNavigationView =  findViewById(R.id.bottom_navigatin_view);
 
-        }
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.explorePage, R.id.mapPage, R.id.profilePage)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.bottomNavigatinView, navController);
+
+//        val navController = findNavController(R.id.nav_fragment);
+//        bottomNavigationView.setupWithNavController(navController);
+    }
 }
+
