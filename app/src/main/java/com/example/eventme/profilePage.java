d5c72397735710988a4331049120be60c9b0c6d6
Mourad Zeynalov <zeynalov@usc.edu>
2022-11-06 10:15:23 -0800

    map changes restored

--- app/src/main/java/com/example/eventme/profilePage.java
+++ app/src/main/java/com/example/eventme/profilePage.java
@@ -18,34 +18,13 @@ public class profilePage extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public profilePage() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profilePage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static profilePage newInstance(String param1, String param2) {
-        profilePage fragment = new profilePage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
