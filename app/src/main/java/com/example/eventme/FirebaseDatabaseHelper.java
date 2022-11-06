ab41846279fde2fdf99ecf66650de8cf48e1cc9d
SriD23 <doupaty@usc.edu>
2022-11-05 23:11:58 -0700

    mapPage

--- app/src/main/java/com/example/eventme/FirebaseDatabaseHelper.java
+++ app/src/main/java/com/example/eventme/FirebaseDatabaseHelper.java
@@ -12,6 +12,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,7 +35,7 @@ public class FirebaseDatabaseHelper {
 
     public FirebaseDatabaseHelper() {
         mDatabase = FirebaseDatabase.getInstance();
-        mReferenceEvents = mDatabase.getReference("events");
+        mReferenceEvents = mDatabase.getReference("Events");
         mReferenceUsers = mDatabase.getReference("User");
     }
 
