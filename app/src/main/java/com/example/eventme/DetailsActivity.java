6b8ad98cfc1333ee9a3d881651d3ce29bd5ea03d
SriD23 <doupaty@usc.edu>
2022-11-06 18:02:59 -0800

    map event page

--- app/src/main/java/com/example/eventme/DetailsActivity.java
+++ app/src/main/java/com/example/eventme/DetailsActivity.java
@@ -1,13 +1,26 @@
 package com.example.eventme;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
 
     TextView markertext;
+    List<EventBox> listOfEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +30,52 @@ public class DetailsActivity extends AppCompatActivity {
 
         String title = getIntent().getStringExtra("title");
 
-        markertext.setText(title);
+
+        FirebaseDatabaseHelper fb = new FirebaseDatabaseHelper();
+        fb.readEvents(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<EventBox> events, List<String> keys) {
+                Toast.makeText(getApplicationContext(), "Data was loaded", Toast.LENGTH_SHORT).show();
+                listOfEvents = events;
+
+                EventBox temp = null;
+                for(EventBox e: listOfEvents){
+
+                    if(e.getName() == title){
+                        temp = e;
+                        break;
+                    }
+
+                }
+
+                String temporary = "";
+                if(temp != null) {
+                    temporary = temp.getDate() + "\n" + temp.getEvent_Type() + temp.getEvent_Description() + "\n";
+                }
+                else{
+                    temporary = title;
+                }
+
+                markertext.setText(temporary);
+
+
+            }
+
+            @Override
+            public void DataIsInserted() {
+            }
+
+            @Override
+            public void DataIsUpdated() {
+            }
+
+            @Override
+            public void DataIsDeleted() {
+            }
+        });
+
+
+
 
 
     }
--- app/src/main/java/com/example/eventme/mapPage.java
+++ app/src/main/java/com/example/eventme/mapPage.java
@@ -1,17 +1,29 @@
 package com.example.eventme;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationRequest;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.eventme.databinding.FragmentMapPageBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,11 +31,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class mapPage extends Fragment {
+public class mapPage extends Fragment{
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -35,6 +49,13 @@ public class mapPage extends Fragment {
     private MarkerOptions options = new MarkerOptions();
     private ArrayList<LatLng> latlngs = new ArrayList<>();
     private ArrayList<Marker> markers = new ArrayList<>();
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location currLocation;
+    FragmentMapPageBinding binding;
+    SupportMapFragment mapFragment;
+    private static final int REQUEST_CODE = 101;
+
+
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -50,6 +71,7 @@ public class mapPage extends Fragment {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        @SuppressLint("MissingPermission")
         @Override
         public void onMapReady(GoogleMap googleMap) {
 
@@ -98,6 +120,27 @@ public class mapPage extends Fragment {
                     return false;
                 }
             });
+
+//            Location loci = fusedLocationClient.getLastLocation().getResult();
+//            LatLng currLoci = new LatLng(loci.getLatitude(), loci.getLongitude());
+//            googleMap.addMarker(new MarkerOptions().position(currLoci).title("Your current location!"));
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener((Activity) getContext(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                LatLng currLoci = new LatLng(location.getLatitude(), location.getLongitude());
+                                googleMap.addMarker(new MarkerOptions().position(currLoci).title("Your current location!"));
+                            }
+                        }
+
+                    });
+
+
+
+            //fetchLastLocation();
+
         }
     };
 
@@ -106,7 +149,6 @@ public class mapPage extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
 
         return inflater.inflate(R.layout.fragment_map_page, container, false);
     }
@@ -123,11 +165,45 @@ public class mapPage extends Fragment {
 
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity().getApplicationContext());
+        fetchLastLocation();
 
 
     }
+
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(location -> {
+            if (location != null) {
+                currLocation = location;
+                UpdateCurrentLocation();
+                Toast.makeText(getContext(), currLocation.getLatitude()
+                        + "" + currLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                SupportMapFragment supportMapFragment = (SupportMapFragment)
+                        getChildFragmentManager().findFragmentById(R.id.map);
+                supportMapFragment.getMapAsync((OnMapReadyCallback) mapPage.this);
+            }
+        });
+    }
+    private void UpdateCurrentLocation() {
+        LatLng latLng = new LatLng(currLocation.getLatitude(),
+                currLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng)
+                .title("Here I am!");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions);
+    }
+
+
 
 }
