a2600de27834919441466ad7942b3ce258b4f00f
Mourad Zeynalov <zeynalov@usc.edu>
2022-11-06 20:01:27 -0800

    ticker event description

--- app/src/main/java/com/example/eventme/DetailsActivity.java
+++ app/src/main/java/com/example/eventme/DetailsActivity.java
@@ -4,23 +4,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
 
     TextView markertext;
     List<EventBox> listOfEvents;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +53,13 @@ public class DetailsActivity extends AppCompatActivity {
 
                 EventBox temp = null;
                 for(EventBox e: listOfEvents){
-
-                    if(e.getName() == title){
+                    String horse = e.getName();
+                    if(horse.equals(title)){
                         temp = e;
                         break;
                     }
 
                 }
-
                 String temporary = "";
                 if(temp != null) {
                     temporary = temp.getDate() + "\n" + temp.getEvent_Type() + temp.getEvent_Description() + "\n";
@@ -57,6 +69,13 @@ public class DetailsActivity extends AppCompatActivity {
                 }
 
                 markertext.setText(temporary);
+                markertext.setText("\n");
+                markertext.setText("testingtestingtesting");
+                String urlImage = temp.getImage_url();
+
+                imageView = findViewById(R.id.image_view1);
+                Glide.with(getApplicationContext()).load(urlImage).into(imageView);
+
 
 
             }
