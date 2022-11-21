7d4fe880b27ac8dc28ee7fef8749002d2eff1476
James Fan <jlfan@usc.edu>
2022-11-20 17:11:09 -0800

    event conflict

---
+++ app/src/androidTest/java/com/example/eventme/EventConflict.java
@@ -0,0 +1,221 @@
+package com.example.eventme;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.example.eventme.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class EventConflict {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void eventConflict() {
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(500);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction materialButton = onView(
+allOf(withId(R.id.Destination), withText("Filter By Nearest"),
+childAtPosition(
+allOf(withId(R.id.frameLayout),
+childAtPosition(
+withId(R.id.nav_fragment),
+0)),
+5),
+isDisplayed()));
+        materialButton.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(700);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction recyclerView = onView(
+allOf(withId(R.id.mRecyclerView),
+childAtPosition(
+withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+0)));
+        recyclerView.perform(actionOnItemAtPosition(12, click()));
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(700);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction materialButton2 = onView(
+allOf(withId(R.id.register), withText("Register For Event"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+3),
+isDisplayed()));
+        materialButton2.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(700);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction appCompatEditText = onView(
+allOf(withId(R.id.idEdtUserName),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+2),
+isDisplayed()));
+        appCompatEditText.perform(replaceText("james@gmail.com"), closeSoftKeyboard());
+        
+        ViewInteraction appCompatEditText2 = onView(
+allOf(withId(R.id.idEdtPassword),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+3),
+isDisplayed()));
+        appCompatEditText2.perform(replaceText("abc123"), closeSoftKeyboard());
+        
+        ViewInteraction materialButton3 = onView(
+allOf(withId(R.id.idBtnLogin), withText("Login"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+1),
+isDisplayed()));
+        materialButton3.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(7000);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction materialButton4 = onView(
+allOf(withId(R.id.Destination), withText("Filter By Nearest"),
+childAtPosition(
+allOf(withId(R.id.frameLayout),
+childAtPosition(
+withId(R.id.nav_fragment),
+0)),
+5),
+isDisplayed()));
+        materialButton4.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(700);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction recyclerView2 = onView(
+allOf(withId(R.id.mRecyclerView),
+childAtPosition(
+withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+0)));
+        recyclerView2.perform(actionOnItemAtPosition(13, click()));
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(700);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction materialButton5 = onView(
+allOf(withId(R.id.register), withText("Register for Event"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+3),
+isDisplayed()));
+        materialButton5.perform(click());
+        
+        ViewInteraction button = onView(
+allOf(withId(R.id.register), withText("OK!"),
+withParent(withParent(withId(android.R.id.content))),
+isDisplayed()));
+        button.check(matches(isDisplayed()));
+        }
+    
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup)parent).getChildAt(position));
+            }
+        };
+    }
+    }
